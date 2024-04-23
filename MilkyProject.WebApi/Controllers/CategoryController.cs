21f060f806f0d2a10ca847632450a5f2cffcde90
MuratYucedag <yucedagmurat23@gmail.com>
2024-04-23 16:20:28 +0300

    ...

---
+++ MilkyProject.WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MilkyProject.BusinessLayer.Abstract;
+using MilkyProject.EntityLayer.Concrete;
+
+namespace MilkyProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _categoryService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _categoryService.TInsert(category);
+            return Ok("Kategori Başarıyla Eklendi");
+        }
+    }
+}

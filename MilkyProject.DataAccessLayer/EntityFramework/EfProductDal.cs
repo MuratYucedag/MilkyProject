0a123344097c70e84cace0574ca5589bf182ba32
MuratYucedag <yucedagmurat23@gmail.com>
2024-04-23 15:34:04 +0300

    ..

--- MilkyProject.DataAccessLayer/EntityFramework/EfProductDal.cs
+++ MilkyProject.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MilkyProject.DataAccessLayer.EntityFramework
 {
     public class EfProductDal : GenericRepository<Product>, IProductDal
